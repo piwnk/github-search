2ba22ba35a7a6063f3bd38b871343013b3bbc193
Piotr Iwaniuk <piwaniuk@esri.pl>
2017-11-08 15:30:55 +0100

    fix: proptypes added to UserList

--- components/UsersList.jsx
+++ components/UsersList.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 import User from './User';
 
 export default class UsersList extends Component {
-   // static propTypes = {
-   //    users: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired
-   // }
+   static propTypes = {
+      users: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.oneOfType([
+         PropTypes.string,
+         PropTypes.number,
+         PropTypes.bool
+      ]))).isRequired
+   }
 
    get users() {
       return this.props.users.map(user => (
